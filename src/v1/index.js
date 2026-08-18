8e2de1473f62efb51460cc63778c46c06048705e
Jamie Achatz <jamieachatz@gmail.com>
2026-08-18 13:44:58 +0200

    Add health and products routes to API

--- src/v1/index.js
+++ src/v1/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const health = require('./health');
+const products = require('./fms/products');
+
+router.use('/health', health);
+router.use('/fms/products', products);
 
 router.get('/', (req, res) => {
     res.json({
-        message: 'Welcome to the All API!',
+        message: 'Welcome to the All API v1!',
         version: '1.0.0',
         endpoints: {
             'cross endpoints': {
