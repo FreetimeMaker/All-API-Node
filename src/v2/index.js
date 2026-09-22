c5ded81c50503734c8010586a2c877c18aa29252
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-22 14:06:40 +0200

    Update index.js

--- src/v2/index.js
+++ src/v2/index.js
@@ -41,7 +41,7 @@ router.use('/blog', asRouter(blogRoutes));
 router.get('/v2', (req, res) => {
     res.json({
         message: 'Welcome to the All API v2!',
-        version: '2.6.1',
+        version: '2.7.0',
         endpoints: {
             'cross endpoints': {
                 health: '/health',
